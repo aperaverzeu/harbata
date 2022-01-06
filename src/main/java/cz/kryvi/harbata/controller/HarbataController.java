aa995cb8dda9394f5e6626cbbabc202a966153dc
Alaksiej <aspheflaneur@gmail.com>
2022-01-06 22:36:48 +0300

    refactoring

--- src/main/java/cz/kryvi/harbata/controller/HarbataController.java
+++ src/main/java/cz/kryvi/harbata/controller/HarbataController.java
@@ -5,23 +5,24 @@ import cz.kryvi.harbata.model.entity.Harbata;
 import cz.kryvi.harbata.model.mapper.HarbataMapper;
 import cz.kryvi.harbata.service.HarbataService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.annotation.*;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping(value = "/api/harbata")
 public record HarbataController(HarbataService harbataService,
                                 HarbataMapper harbataMapper) {
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
     public Mono<Harbata> getItemById(@PathVariable Long id) {
         return harbataService.getOne(id);
     }
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
     public Flux<Harbata> getAll() {
         return harbataService.getAll();
     }
--- src/main/java/cz/kryvi/harbata/service/HarbataService.java
+++ src/main/java/cz/kryvi/harbata/service/HarbataService.java
@@ -2,35 +2,58 @@ package cz.kryvi.harbata.service;
 
 import cz.kryvi.harbata.model.entity.Harbata;
 import cz.kryvi.harbata.repository.HarbataRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
 public record HarbataService(HarbataRepository harbataRepository) {
 
     public Mono<Harbata> getOne(Long id) {
-        return harbataRepository.findById(id);
+        return harbataRepository
+                .findById(id)
+                .doOnError(throwable -> {
+                    log.error("Oops! Error here. It seems we don't have this harbata element");
+                    throw new IllegalStateException(throwable);
+                });
     }
 
     public Flux<Harbata> getAll() {
-        return harbataRepository.findAll();
+        return harbataRepository
+                .findAll()
+                .doOnError(throwable -> {
+                    log.error("Oops! Error here. It seems we don't have these harbata elements");
+                    throw new IllegalStateException(throwable);
+                });
     }
 
     public Mono<Harbata> save(Harbata harbata) {
-        return harbataRepository.save(harbata);
+        return harbataRepository
+                .save(harbata)
+                .doOnError(throwable -> {
+                    log.error("Oops! Error here. It seems we cannot save this harbata element");
+                    throw new IllegalStateException(throwable);
+                });
     }
 
     public Mono<Harbata> update(Harbata harbata) {
         return harbataRepository
                 .findById(harbata.getId())
-                .flatMap(harbataRepository::save)
                 .doOnError(throwable -> {
-                    throw new IllegalStateException();
-                });
+                    log.error("Oops! Error here. It seems we cannot update this harbata element");
+                    throw new IllegalStateException(throwable);
+                })
+                .flatMap(harbataRepository::save);
     }
 
     public Mono<Void> delete(Long id) {
-        return harbataRepository.deleteById(id);
+        return harbataRepository
+                .deleteById(id)
+                .doOnError(throwable -> {
+                    log.error("Oops! Error here. It seems we cannot delete this harbata element");
+                    throw new IllegalStateException(throwable);
+                });
     }
 }
