b0d65d28e48e782dcd8941218dacfa2a49e6acf4
Alaksiej <aspheflaneur@gmail.com>
2022-01-20 03:02:01 +0300

    update service functionality

--- src/main/java/cz/kryvi/harbata/service/HarbataService.java
+++ src/main/java/cz/kryvi/harbata/service/HarbataService.java
@@ -2,67 +2,38 @@ package cz.kryvi.harbata.service;
 
 import cz.kryvi.harbata.model.entity.Harbata;
 import cz.kryvi.harbata.repository.HarbataRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Slf4j
 @Service
 public record HarbataService(HarbataRepository harbataRepository) {
 
-    public Mono<Harbata> getOne(Long id) {
-        return harbataRepository
-                .findById(id)
-                .doOnError(throwable -> {
-                    log.error("Oops! Error here. It seems we don't have this harbata element");
-                    throw new IllegalStateException(throwable);
-                });
+    public Mono<Harbata> getOneById(Long id) {
+        return harbataRepository.findById(id);
     }
 
     public Mono<Harbata> getOneByName(String name) {
-        return harbataRepository
-                .findHarbataByName(name)
-                .doOnError(throwable -> {
-                    log.error("Oops! Error here. It seems we don't have this harbata element");
-                    throw new IllegalStateException(throwable);
-                });
+        return harbataRepository.findHarbataByName(name);
     }
 
     public Flux<Harbata> getAll() {
-        return harbataRepository
-                .findAll()
-                .doOnError(throwable -> {
-                    log.error("Oops! Error here. It seems we don't have these harbata elements");
-                    throw new IllegalStateException(throwable);
-                });
+        return harbataRepository.findAll();
     }
 
     public Mono<Harbata> save(Harbata harbata) {
-        return harbataRepository
-                .save(harbata)
-                .doOnError(throwable -> {
-                    log.error("Oops! Error here. It seems we cannot save this harbata element");
-                    throw new IllegalStateException(throwable);
-                });
+        return harbataRepository.save(harbata);
     }
 
     public Mono<Harbata> update(Harbata harbata) {
         return harbataRepository
-                .findById(harbata.getId())
-                .doOnError(throwable -> {
-                    log.error("Oops! Error here. It seems we cannot update this harbata element");
-                    throw new IllegalStateException(throwable);
-                })
-                .flatMap(harbataRepository::save);
+                .existsById(harbata.getId())
+                .flatMap(exists -> exists.equals(Boolean.TRUE)
+                        ? harbataRepository.save(harbata)
+                        : Mono.empty());
     }
 
     public Mono<Void> delete(Long id) {
-        return harbataRepository
-                .deleteById(id)
-                .doOnError(throwable -> {
-                    log.error("Oops! Error here. It seems we cannot delete this harbata element");
-                    throw new IllegalStateException(throwable);
-                });
+        return harbataRepository.deleteById(id);
     }
 }
