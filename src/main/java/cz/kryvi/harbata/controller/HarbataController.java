f7575b1159cbff368b7d34af85d4b2f04d70a1af
Alaksiej <aspheflaneur@gmail.com>
2022-01-20 03:02:29 +0300

    add ResponseEntity support to Controller

--- src/main/java/cz/kryvi/harbata/controller/HarbataController.java
+++ src/main/java/cz/kryvi/harbata/controller/HarbataController.java
@@ -5,6 +5,7 @@ import cz.kryvi.harbata.model.entity.Harbata;
 import cz.kryvi.harbata.model.mapper.HarbataMapper;
 import cz.kryvi.harbata.service.HarbataService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.*;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -18,28 +19,37 @@ public record HarbataController(HarbataService harbataService,
                                 HarbataMapper harbataMapper) {
 
     @GetMapping(value = "/{id}", produces = APPLICATION_JSON_VALUE)
-    public Mono<Harbata> getItemById(@PathVariable Long id) {
-        return harbataService.getOne(id);
+    public Mono<ResponseEntity<Harbata>> getItemById(@PathVariable Long id) {
+        return harbataService.getOneById(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
-    public Flux<Harbata> getAll() {
-        return harbataService.getAll();
+    public Flux<ResponseEntity<Harbata>> getAll() {
+        return harbataService.getAll()
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    @ResponseStatus(value = HttpStatus.CREATED)
-    public Mono<Harbata> save(@RequestBody @Validated HarbataDTO harbata) {
-        return harbataService.save(harbataMapper.apply(harbata));
+    public Mono<ResponseEntity<Harbata>> save(@RequestBody @Validated HarbataDTO harbata) {
+        return harbataService.save(harbataMapper.apply(harbata))
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
 
     @PutMapping
-    public Mono<Harbata> update(@RequestBody @Validated HarbataDTO harbata) {
-        return harbataService.update(harbataMapper.apply(harbata));
+    public Mono<ResponseEntity<Harbata>> update(@RequestBody @Validated HarbataDTO harbata) {
+        return harbataService.update(harbataMapper.apply(harbata))
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
 
     @DeleteMapping("/{id}")
-    public Mono<Void> deleteItemById(@PathVariable Long id) {
-        return harbataService.delete(id);
+    public Mono<ResponseEntity<Void>> deleteItemById(@PathVariable Long id) {
+        return harbataService.delete(id)
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
