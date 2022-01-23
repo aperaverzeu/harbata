17a12b0ce6b8be3c44fb226e9265e2009cab5037
Alaksiej <aspheflaneur@gmail.com>
2022-01-23 04:37:27 +0300

    fix docker-compose bug

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-cd ..
-mvn clean install -DskipTests
+# shellcheck disable=SC2164
+cd ../backend/slounik
+mvn package
 
 # shellcheck disable=SC2164
-cd docker
+cd ../..
 docker-compose up -d mongo
 sleep 5
-docker-compose up -d --build app
+docker-compose up -d --build slounik
