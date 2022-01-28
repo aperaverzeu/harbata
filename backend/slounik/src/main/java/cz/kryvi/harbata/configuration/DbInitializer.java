884ee8bbd09f17e414878a34408341458dbf5304
Alaksiej <aspheflaneur@gmail.com>
2022-01-28 23:38:22 +0300

    refactoring

--- backend/slounik/src/main/java/cz/kryvi/harbata/configuration/DbInitializer.java
+++ backend/slounik/src/main/java/cz/kryvi/harbata/configuration/DbInitializer.java
@@ -2,7 +2,6 @@ package cz.kryvi.harbata.configuration;
 
 import cz.kryvi.harbata.model.entity.Harbata;
 import cz.kryvi.harbata.repository.HarbataRepository;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,23 +11,26 @@ import java.util.List;
 
 @EnableReactiveMongoRepositories(basePackageClasses = HarbataRepository.class)
 @Configuration
-@Slf4j
 public class DbInitializer {
     @Bean
     CommandLineRunner commandLineRunner(HarbataRepository harbataRepository) {
-        return args -> harbataRepository.saveAll(List.of(
-                        new Harbata(1L, "Гарбата", "Harbata", """
-                                Вечна зялёная расьліна зь віду камэлія, якая паходзіць з паўднёва-ўсходняй Азіі,
-                                а таксама духмяны напой, які звычайна атрымліваюць шляхам заліваньня гарачай вадой або
-                                кіпенем высушанага лісьця гэтай расьліны. Пасьля вады, гарбата зьяўляецца найбольш
-                                шырокаспажываным напоем у сьвеце. Яна мае зьлёгку горкі і вязкі водар"""),
-                        new Harbata(2L, "Імбрык", "Imbryk", """
-                                Невялічкая кухонная прылада для запарваньня гарбаты ці іншых напояў у гарачай вадзе.
-                                Звычайна імбрычкі маюць зьверху адтуліну з накрыўкай, празь якую засыпаецца гарбата і
-                                заліваецца вада; ручку дзеля трыманьня рукой; і рыльца, адкуль наліваюць запараную
-                                гарбату. У некаторых выпадках унутры імбрычка можа зьмяшчацца сітца, каб працэжваць
-                                лісьце гарбаты пры наліваньні"""))
-                )
+
+        var dataset = List.of(
+                new Harbata(1L, "Гарбата", "Harbata", """
+                        Вечна зялёная расьліна зь віду камэлія, якая паходзіць з паўднёва-ўсходняй Азіі,
+                        а таксама духмяны напой, які звычайна атрымліваюць шляхам заліваньня гарачай вадой або
+                        кіпенем высушанага лісьця гэтай расьліны. Пасьля вады, гарбата зьяўляецца найбольш
+                        шырокаспажываным напоем у сьвеце. Яна мае зьлёгку горкі і вязкі водар"""),
+                new Harbata(2L, "Імбрык", "Imbryk", """
+                        Невялічкая кухонная прылада для запарваньня гарбаты ці іншых напояў у гарачай вадзе.
+                        Звычайна імбрычкі маюць зьверху адтуліну з накрыўкай, празь якую засыпаецца гарбата і
+                        заліваецца вада; ручку дзеля трыманьня рукой; і рыльца, адкуль наліваюць запараную
+                        гарбату. У некаторых выпадках унутры імбрычка можа зьмяшчацца сітца, каб працэжваць
+                        лісьце гарбаты пры наліваньні"""));
+
+        return args -> harbataRepository
+                .saveAll(dataset)
+                .log()
                 .subscribe();
     }
 }
